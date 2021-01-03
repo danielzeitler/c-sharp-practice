3897856a7ac55aa84db89dea9ddaedfb31b00c80
Daniel Zeitler <daniel@mieli.de>
2021-01-03 17:06:03 +0100

    first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args)
+        {
+            const float Pi = 3.14f;
+            Console.WriteLine(Pi);
+        }
+    }
+}
