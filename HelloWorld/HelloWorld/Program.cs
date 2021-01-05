0d2fa20ce1d92a3ce062ce3d88fd2e916dd4751d
Daniel Zeitler <daniel@mieli.de>
2021-01-05 19:58:51 +0100

    primitive types

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,8 +4,20 @@ namespace HelloWorld
 {
     class Program
     {
+        public enum Shipping
+        {
+            RegularMail = 1,
+            RegisteredMail = 2,
+            Express = 6
+        }
+
         static void Main(string[] args)
         {
+            
+            // Console.WriteLine((Shipping) 1);
+
+            Console.WriteLine((Enum.Parse(typeof(Shipping), "6")));
+            
             int[] numbers = new int[3] {4, 6, 7};
             string[] somenames = new string[3] {"hello", "my name is ", "john doe"};
             // Console.WriteLine(numbers[0]);
@@ -16,11 +28,12 @@ namespace HelloWorld
             String fullname = String.Format("{0} {1}", firstname, lastname);
 
             // join array 
-            String joinedArray = String.Join(".", somenames);
-            for (int i = 0; i < somenames.Length; i++)
+            String joinedArray = String.Join(" - ", somenames);
+            Console.WriteLine(joinedArray);
+            /*for (int i = 0; i < somenames.Length; i++)
             {
                 Console.WriteLine(somenames[i]);
-            }
+            }*/
 
             Person person = new Person("daniel", "testuser");
             // person.introduce();
