334de06469ddd59ab1f7bee702442ddaca125657
Daniel Zeitler <daniel@mieli.de>
2021-01-05 19:40:21 +0100

    non primitive types

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,14 +1,32 @@
-﻿namespace HelloWorld {
-    class Program {
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
         static void Main(string[] args)
         {
+            int[] numbers = new int[3] {4, 6, 7};
+            string[] somenames = new string[3] {"hello", "my name is ", "john doe"};
+            // Console.WriteLine(numbers[0]);
+
+            // concat strings 
+            String firstname = "John";
+            String lastname = "Doe";
+            String fullname = String.Format("{0} {1}", firstname, lastname);
+
+            // join array 
+            String joinedArray = String.Join(".", somenames);
+            for (int i = 0; i < somenames.Length; i++)
+            {
+                Console.WriteLine(somenames[i]);
+            }
+
             Person person = new Person("daniel", "testuser");
-            person.introduce();
+            // person.introduce();
 
             Car.Car car = new Car.Car(4, "Racer");
             car.BuildCar();
         }
     }
 }
-
-
