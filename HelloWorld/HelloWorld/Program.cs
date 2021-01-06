34cbf391cd84aab58b05a0a069a4b5ca9a32000e
Daniel Zeitler <daniel@mieli.de>
2021-01-06 12:14:51 +0100

    array and lists

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,10 +4,31 @@ namespace HelloWorld
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-           
+            // 3 rows, 5 columns
+            // var matrix = new int[3, 5];
+            var matrix = new int[3, 5]
+            {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15}
+            };
+
+            // Jagged array
+            // var array = new int[3][];
+            
+            int[] numbers = new int[] {3, 7, 8, 1, 3, 2};
+            int index = Array.IndexOf(numbers, 7);
+            Console.WriteLine(index);
+            
+            Array.Clear(numbers, 0, 3);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            
+
         }
     }
 }
